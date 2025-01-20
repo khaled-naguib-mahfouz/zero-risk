cbc36e1f2f2c8266cc4ab266c3484d32a91f0130
Khaled Naguib <Khaled.Naguib@initiumsolutions.me>
2025-01-20 15:51:07 +0200

    finished home

--- script.js
+++ script.js
@@ -12,3 +12,42 @@ console.log("ss");
         }
     });
 });
+
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+// When the user scrolls down 100px from the top, show the button
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopBtn.classList.add('show');
+    } else {
+        scrollToTopBtn.classList.remove('show');
+    }
+};
+
+// When the user clicks the button, scroll to the top
+scrollToTopBtn.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+// function reorderDivs() {
+//     console.log("dsadsa");
+//     let screenSize = window.matchMedia("(max-width: 991px)"); // Medium screens (Bootstrap md)
+//     let studioRow = document.getElementById("studioRow");
+//     let firstDiv = document.getElementById("firstDiv");
+//     let secondDiv = document.getElementById("secondDiv");
+
+//     if (screenSize.matches) {
+//       if (studioRow.firstChild !== secondDiv) {
+//         studioRow.insertBefore(secondDiv, firstDiv);
+//       }
+//     } else {
+//       if (studioRow.firstChild !== firstDiv) {
+//         studioRow.insertBefore(firstDiv, secondDiv);
+//       }
+//     }
+//   }
+
+//   // Run on page load
+//   reorderDivs();
+
+//   // Run on window resize
+//   window.addEventListener("resize", reorderDivs);
